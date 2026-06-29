6eb66328b616163912e38cf327e964eab1a18c11
JayceDevelops <jayceiezzi@gmail.com>
2026-06-29 11:48:21 -0400

    Game finished

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,23 @@
 import '../styles/Card.css';
 
-export default function Card({image, title, description}) {
+export default function Card({id, image, title, description, usedID, setUsedID, current, setCurrent, best, setBest, setCards}) {
+
+    const cardClicked = () => {
+        if (usedID.includes(id)){
+            setBest(current);
+            setCurrent(0);
+            setUsedID([]);
+            setCards([]);
+        }else {
+            setCurrent(current + 1);
+            setUsedID((prevUsedID) => [...prevUsedID, id]);
+            setCards([]);
+        }
+
+    }
+
     return (
-        <div className="card">
+        <div className="card" onClick={cardClicked}>
             <img src={image}/>
             <div className="details">
                 <span>{title}</span>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,14 +7,17 @@ export default function Game({current, setCurrent, best, setBest}) {
     const [cards, setCards] = useState([]);
     const [usedID, setUsedID] = useState([]);
 
-    getPokimonData(setCards, setUsedID);
+    getPokimonData(setCards, setUsedID, best, current);
+
+    console.log(cards);
+    console.log(usedID);
 
     return (
         <main>
             <div className='cardholder'>
                 {cards.map((pokemon) => {
                     return (
-                        <Card title={pokemon.name} image={pokemon.image} description={pokemon.description}/>
+                        <Card key={crypto.randomUUID()} id={pokemon.id} title={pokemon.name} image={pokemon.image} description={pokemon.description} usedID={usedID} setUsedID={setUsedID} current={current} setCurrent={setCurrent} best={best} setBest={setBest} setCards={setCards}/>
                     );
                 })}
             </div>
@@ -22,17 +25,19 @@ export default function Game({current, setCurrent, best, setBest}) {
     );
 };
 
-const getPokimonData = (setCards, setUsedID) => {
+const getPokimonData = (setCards, setUsedID, best, current) => {
     useEffect(() => {
         let ignore = false;
 
         const fetchCards = async () => {
 
             try {
-                for (let i = 0; i < 11; i++){
+                let tempCards = [];
+
+                for (let i = 0; i < 18; i++){
 
                     if (ignore === false){
-                        const number = Math.floor(Math.random() * 100) + 1;
+                        const number = Math.floor(Math.random() * 100);
                     
                         const pokemonPromise = fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
                         const speciesPromise = fetch(`https://pokeapi.co/api/v2/pokemon-species/${number}`);
@@ -53,10 +58,11 @@ const getPokimonData = (setCards, setUsedID) => {
                             description: englishDescription?.flavor_text || "No description available."
                         };
 
-                        setCards((prevCards) => [...prevCards, card]);
-                        setUsedID((prevUsedID) => [...prevUsedID, pokemonData.id]);
+                        tempCards.push(card);
                     }
                 }
+
+                setCards(tempCards);
             }
             catch {
                 console.log("Error: Fetching Data");
@@ -69,5 +75,5 @@ const getPokimonData = (setCards, setUsedID) => {
             ignore = true;
         };
 
-    }, []);
+    }, [current, best]);
 };
